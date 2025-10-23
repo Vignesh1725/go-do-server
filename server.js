ecc4193b05dcf4211d419753f9276dea964715ea
Vignesh1725 <vigneshkumaran2315@gmail.com>
2025-10-23 09:16:48 +0530

    Server.js is changed

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/users", userRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("MongoDB Connected");
     })
